dde33b3ed0b419ec0a83e70c835a68cab6f18d8f
gaollg <gaollg@qq.com>
2024-01-19 22:23:41 +0800

    feat: add web

--- lesson5/mysite/src/mysite_frontend/src/index.js
+++ lesson5/mysite/src/mysite_frontend/src/index.js
@@ -1,19 +1,33 @@
 import { mysite_backend } from "../../declarations/mysite_backend";
 
+let reload = async () => {
+  let list = await mysite_backend.timeline(0);
+  let array = [];
+  list.forEach(item => {
+    array.push(`
+      <div>
+        <div>${item.text}</div>
+        <div>${item.time}</div>
+        <div>${item.author}</div>
+      </div>
+    `);
+  });
+  document.getElementById("content").innerHTML = array.join("<hr/>");
+}
+reload();
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+  const input = document.getElementById("input").value.toString();
 
   button.setAttribute("disabled", true);
 
   // Interact with foo actor, calling the greet method
-  const greeting = await mysite_backend.greet(name);
+  await mysite_backend.post(input);
+  await reload();
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
   return false;
 });
